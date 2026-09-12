467a4cda3b26ca0c5e82322de46863eaf7f517f5
Franck Barry <jaimsmuzik@gmail.com>
2026-09-12 14:29:42 +0200

    feat: add storage

---
+++ storage.tf
@@ -0,0 +1,25 @@
+resource "azurerm_storage_account" "files" {
+  name                     = "stecffilesfbarry2026"
+  resource_group_name      = data.azurerm_resource_group.main.name
+  location                 = data.azurerm_resource_group.main.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+}
+
+resource "azurerm_storage_share" "employes" {
+  name                 = "partage-employes"
+  storage_account_name = azurerm_storage_account.files.name
+  quota                = 50
+}
+
+data "azuread_group" "employes" {
+  display_name     = "employes"
+  security_enabled = true
+}
+
+resource "azurerm_role_assignment" "employes_smb" {
+  scope                = azurerm_storage_account.files.id
+  role_definition_name = "Storage File Data SMB Share Contributor"
+  principal_id         = data.azuread_group.employes.object_id
+}
