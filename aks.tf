7917a54544d127119dea83ec79633d47cbd2ff25
Franck Barry <jaimsmuzik@gmail.com>
2026-09-13 20:51:39 +0200

    Move SQL admin password to a variable and fix AKS config

--- aks.tf
+++ aks.tf
@@ -3,12 +3,17 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   dns_prefix          = "aksecfstockage"
+  oidc_issuer_enabled = true
 
   default_node_pool {
     name           = "default"
     node_count     = 2
     vm_size        = "Standard_D2s_v3"
     vnet_subnet_id = azurerm_subnet.aks_subnet.id
+
+    upgrade_settings {
+      max_surge = "10%"
+    }
   }
 
   identity {
