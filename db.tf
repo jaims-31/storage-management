f439ed2e09a3f842ca5d128b0f9d01aa2b4414c4
Franck Barry <jaimsmuzik@gmail.com>
2026-09-13 14:28:45 +0200

    feat: add managed sql server and database infrastructure

---
+++ db.tf
@@ -0,0 +1,14 @@
+resource "azurerm_mssql_server" "sql" {
+  name                         = "sqlserverfbarry2026"
+  resource_group_name          = data.azurerm_resource_group.rg.name
+  location                     = data.azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "sqladminuser"
+  administrator_login_password = "ComplexPassword123!"
+}
+
+resource "azurerm_mssql_database" "db" {
+  name      = "ecf-database"
+  server_id = azurerm_mssql_server.sql.id
+  sku_name  = "Basic"
+}
